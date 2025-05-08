87cfea09be15c13b7a251ea392aa808f777934e0
ChrisBotos <hcty02@gmail.com>
2025-05-08 15:18:58 +0200

    Fixed path related bugs.

--- tests/nuclei_segmentation_tests/pipeline_test.py
+++ tests/nuclei_segmentation_tests/pipeline_test.py
@@ -55,7 +55,7 @@ def test_pipeline_generates_outputs(minimal_settings, dummy_params, dummy_dirs):
 
     assert result == 0
     output = Path(minimal_settings["OUTPUT_DIR"])
-    assert (output / "masks.npy").exists()
-    assert (output / "flows.npz").exists()
-    assert (output / "segmentation_masks.tif").exists()
+    assert (output / "masks" / "segmentation_masks.npy").exists()
+    assert (output / "masks" / "segmentation_masks.tif").exists()
+    assert (output / "flows" / "flows.npz").exists()
     snap.capture.assert_called_once_with("end_of_pipeline", {"masks": dummy_mask})
